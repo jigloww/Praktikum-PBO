fe4e22a06ac1b5d297f17ff94ef9778cbd3c0dd4
unknown <bima.rafi60@sma.belajar.id>
2025-09-04 22:41:51 +0700

    day2-prak

---
+++ Day2/MahasiswaDemo09.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class MahasiswaDemo09 {
+    public static void main(String[] args) {
+        Mahasiswa09 m1 = new Mahasiswa09();
+        m1.nim = "023432";
+        m1.nama = "Yansy Ayuningtyas";
+        m1.alamat = "Nias, Sumatera Utara";
+        m1.kelas = "2A";
+
+        m1.displayBiodata();
+    }
+}
