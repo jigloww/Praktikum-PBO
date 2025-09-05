3fadfaab1a2c03e337bb68b48dcdb1c15a2d6e62
unknown <bima.rafi60@sma.belajar.id>
2025-09-05 10:03:23 +0700

    day2-push5

---
+++ Day2/DemoKipas09.java
@@ -0,0 +1,32 @@
+public class DemoKipas09 {
+    public static void main(String[] args) {
+     
+        Kipas09 kipas1 = new Kipas09();
+        kipas1.merk = "Cosmos";
+        kipas1.warna = "Putih";
+        kipas1.harga = 250000;
+        kipas1.ukuran = 16;
+        kipas1.watt = 45;
+
+        Kipas09 kipas2 = new Kipas09();
+        kipas2.merk = "Miyako";
+        kipas2.warna = "Hitam";
+        kipas2.harga = 300000;
+        kipas2.ukuran = 18;
+        kipas2.watt = 55;
+
+        kipas1.nyalakan();
+        kipas1.tambahKecepatan(2);
+        kipas1.kurangiKecepatan(1);
+        kipas1.displayInfo();
+        kipas1.matikan();
+
+        kipas2.nyalakan();
+        kipas2.tambahKecepatan(3);
+        kipas2.tambahKecepatan(2);
+        kipas2.kurangiKecepatan(4);
+        kipas2.displayInfo();
+        kipas2.matikan();
+    }
+}
+
---
+++ Day2/Kipas09.java
@@ -0,0 +1,55 @@
+public class Kipas09 {
+    public String merk;
+    public String warna;
+    public int harga;
+    public int ukuran;
+    public int watt;
+    public int kecepatan = 0;
+    public boolean status = false;
+
+    public void nyalakan() {
+        status = true;
+        System.out.println(merk + " berhasil dinyalakan.");
+    }
+
+    public void matikan() {
+        status = false;
+        kecepatan = 0; 
+        System.out.println(merk + " berhasil dimatikan.");
+        System.out.println();
+    }
+
+    public int tambahKecepatan(int increment) {
+        if (status) {
+            kecepatan += increment;
+            System.out.println(">   Menambah Kecepatan");
+            System.out.println("Kecepatan sekarang: " + kecepatan);
+        } else {
+            System.out.println("Kipas harus dinyalakan dulu!");
+        }
+        return kecepatan;
+    }
+
+    public int kurangiKecepatan(int decrement) {
+        if (status) {
+            kecepatan -= decrement;
+            if (kecepatan < 0) kecepatan = 0;
+            System.out.println(">   Mengurangi kecepatan");
+            System.out.println("Kecepatan sekarang: " + kecepatan);
+        } else {
+            System.out.println("Kipas harus dinyalakan dulu!");
+        }
+        return kecepatan;
+    }
+
+    public void displayInfo() {
+        System.out.println("Merk     : " + merk);
+        System.out.println("Warna    : " + warna);
+        System.out.println("Harga    : " + harga);
+        System.out.println("Ukuran   : " + ukuran + " inci");
+        System.out.println("Daya     : " + watt + " watt");
+        System.out.println("Status   : " + (status ? "Nyala" : "Mati"));
+        System.out.println("Kecepatan: " + kecepatan);
+    }
+}
+
