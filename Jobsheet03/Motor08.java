2086a1edefd2bda05e07ff33fb6e2a9742c3377e
unknown <bima.rafi60@sma.belajar.id>
2025-09-12 09:56:14 +0700

    7

--- Jobsheet03/Motor08.java
+++ Jobsheet03/Motor08.java
@@ -39,13 +39,17 @@ public class Motor08 {
     }
 
     public void setKecepatan(int kecepatan) {
-    if (!this.isMesinOn && kecepatan > 0) {
-        System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
-    } else if (kecepatan > 100) {
-        System.out.println("Kecepatan maksimal adalah 100!");
-        this.kecepatan = 100;
-    } else {
-        this.kecepatan = kecepatan;
+        if (!this.isMesinOn && kecepatan > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else if (kecepatan > 100) {
+            System.out.println("Kecepatan maksimal adalah 100!");
+            this.kecepatan = 100;
+        } else if (kecepatan < 0) {
+            System.out.println("Kecepatan tidak boleh negatif!");
+            this.kecepatan = 0;
+        } else {
+            this.kecepatan = kecepatan;
+        }
     }
-    }
+
 }
