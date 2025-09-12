e5e480b0d4fa3bb2581266d286990732c9257452
unknown <bima.rafi60@sma.belajar.id>
2025-09-12 09:29:47 +0700

    5

---
+++ Jobsheet03/DemoUser08.java
@@ -0,0 +1,9 @@
+package Jobsheet03;
+
+public class DemoUser08 {
+    public static void main(String[] args) {
+        User08 user1 = new User08("annisa.nadya", "Annisa Nadya", "annisa.nadya@gmail.com");
+
+        user1.cetakInfo();
+    }
+}
