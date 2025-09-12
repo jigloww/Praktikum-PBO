26a0a57f09885bc73297254891c029ed97abbd0f
unknown <bima.rafi60@sma.belajar.id>
2025-09-12 08:51:49 +0700

    2

--- Jobsheet03/MotorDemo08.java
+++ Jobsheet03/MotorDemo08.java
@@ -6,18 +6,39 @@ public class MotorDemo08 {
         motor1.displayStatus();
 
         motor1.platNomor = "B 0838 XZ";
-        motor1.kecepatan = 50;
+        
+        int kecepatanBaru = 50;
+
+        if(!motor1.isMesinOn && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else {
+            motor1.kecepatan = kecepatanBaru;
+        }
+
         motor1.displayStatus();
 
         Motor08 motor2 = new Motor08();
         motor2.platNomor = "N 9840 AB";
         motor2.isMesinOn = true;
-        motor2.kecepatan = 40;
+        kecepatanBaru = 40;
+
+        if(!motor2.isMesinOn && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else {
+            motor2.kecepatan = kecepatanBaru;
+        }
+
         motor2.displayStatus();
 
         Motor08 motor3 = new Motor08();
-        motor1.platNomor = "D 8343 CV";
-        motor1.kecepatan = 60;
-        motor1.displayStatus();
+        motor3.platNomor = "D 8343 CV";
+        kecepatanBaru = 60;
+
+        if(!motor3.isMesinOn && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else {
+            motor3.kecepatan = kecepatanBaru;
+        }
+        motor3.displayStatus();
     }
 }
