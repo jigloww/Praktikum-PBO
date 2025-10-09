f975959cae55cd67f164689b3fd4da7f60837b00
unknown <bima.rafi60@sma.belajar.id>
2025-10-09 20:24:23 +0700

    percobaan 3#2

--- Day6/Pegawai.java
+++ Day6/Pegawai.java
@@ -3,7 +3,7 @@ package Day6;
 public class Pegawai {
     public String nip;
     public String nama;
-    private double gaji;
+    protected double gaji;
 
     public Pegawai() {
         System.out.println("Objek dari class Pegawai dibuat");
