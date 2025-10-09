efe39cc49613a2c8e7b2990be6008dbd57ea09bf
unknown <bima.rafi60@sma.belajar.id>
2025-10-09 20:32:17 +0700

    percobaan 3#3

--- Day6/Dosen.java
+++ Day6/Dosen.java
@@ -1,9 +1,11 @@
 package Day6;
+import Day6.Pegawai;
 
 public class Dosen extends Pegawai {
     public String nidn;
     
     public Dosen() {
+        System.out.println(gaji);
         System.out.println("Objek dari class Dosen dibuat");
     }
 }
