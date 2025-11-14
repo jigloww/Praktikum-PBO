abe4a090e0a3493bd2db24f77dd8d6f1ac8dcce5
unknown <bima.rafi60@sma.belajar.id>
2025-11-14 13:25:12 +0700

    Objek Cast3

--- JS11/DemoPegawaiMain08.java
+++ JS11/DemoPegawaiMain08.java
@@ -2,20 +2,25 @@ import java.util.ArrayList;
 
 public class DemoPegawaiMain08 {
     public static void main(String[] args) {
+
         Dosen08 dosen1 = new Dosen08("19940201", "Widia, S.Kom. M.Kom", "199402");
-
-        System.out.println(dosen1.nip);
-        System.out.println(dosen1.nama);
-        System.out.println(dosen1.nidn);
-        dosen1.mengajar();
-        System.out.println();
-
         Pegawai08 pegawai1 = dosen1;
+
         System.out.println(pegawai1.nip);
         System.out.println(pegawai1.nama);
         pegawai1.displayInfo();
-        System.out.println();
 
+        Dosen08 newDosen08 = (Dosen08) pegawai1;
+
+        System.out.println(newDosen08.nama);
+        System.out.println(newDosen08.nidn);
+        newDosen08.mengajar();
+
+        // System.out.println(dosen1.nip);
+        // System.out.println(dosen1.nama);
+        // System.out.println(dosen1.nidn);
+        // dosen1.mengajar();
+        // System.out.println();
         // Dosen08 dosen2 = new Dosen08("19700105", "Muhammad, S.T. M.T", "197001");
         // TenagaKependidikan08 tendik1 = new TenagaKependidikan08("19750301", "Aida,
         // A.Md.", "Tenaga Administrasi");
